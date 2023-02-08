421be047560b2f26f5118dffce2a8561ed9e0b58
AsuJuve <superjuve@outlook.es>
2023-02-08 13:01:19 -0600

    Aplicación de PEP8 a test_calculadora

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,11 +1,13 @@
 import unittest
 from calculadora import Calculadora
 
+
 class TestCalculadora(unittest.TestCase):
-    
+
     def test_2_as_2(self):
         calc = Calculadora()
         self.assertEqual(4, calc.sumar(2, 2))
+
 
 if __name__ == '__main__':
     unittest.main()
